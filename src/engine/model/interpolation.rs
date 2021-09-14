ee436233aa4aa684d1cc1404b6fb8c964542b962
larsjarlvik <lars.jarlvik@gmail.com>
2021-09-14 20:49:48 +0700

    Use built-in slerp

--- src/engine/model/interpolation.rs
+++ src/engine/model/interpolation.rs
@@ -9,7 +9,6 @@ pub enum Interpolation {
 
 pub trait Interpolate: Copy {
     fn linear(self, other: Self, amount: f32) -> Self;
-
     fn cubic_spline(source: [Self; 3], source_time: f32, target: [Self; 3], target_time: f32, current_time: f32) -> Self;
 }
 
@@ -34,37 +33,7 @@ impl Interpolate for Vector3<f32> {
 
 impl Interpolate for Quaternion<f32> {
     fn linear(self, other: Self, amount: f32) -> Self {
-        let left = self;
-        let right = other;
-
-        let num2;
-        let num3;
-        let num = amount;
-        let mut num4 = (((left.v.x * right.v.x) + (left.v.y * right.v.y)) + (left.v.z * right.v.z)) + (left.s * right.s);
-        let mut flag = false;
-        if num4 < 0.0 {
-            flag = true;
-            num4 = -num4;
-        }
-        if num4 > 0.999_999 {
-            num3 = 1.0 - num;
-            num2 = if flag { -num } else { num };
-        } else {
-            let num5 = num4.acos();
-            let num6 = 1.0 / num5.sin();
-            num3 = ((1.0 - num) * num5).sin() * num6;
-            num2 = if flag {
-                -(num * num5).sin() * num6
-            } else {
-                (num * num5).sin() * num6
-            };
-        }
-        Quaternion::new(
-            (num3 * left.s) + (num2 * right.s),
-            (num3 * left.v.x) + (num2 * right.v.x),
-            (num3 * left.v.y) + (num2 * right.v.y),
-            (num3 * left.v.z) + (num2 * right.v.z),
-        )
+        self.slerp(other, amount)
     }
 
     fn cubic_spline(source: [Self; 3], source_time: f32, target: [Self; 3], target_time: f32, amount: f32) -> Self {
