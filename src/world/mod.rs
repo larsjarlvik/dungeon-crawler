83fd48bcaab5fc522efa9521178e8522fb11d3a8
larsjarlvik <lars.jarlvik@gmail.com>
2022-03-04 21:46:17 +0700

    Remove example code

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -136,44 +136,6 @@ impl<'a> World {
             _ => {}
         }
     }
-
-    /*
-
-       double t = 0.0;
-    double dt = 0.01;
-
-    double currentTime = hires_time_in_seconds();
-    double accumulator = 0.0;
-
-    State previous;
-    State current;
-
-    while ( !quit )
-    {
-        double newTime = time();
-        double frameTime = newTime - currentTime;
-        if ( frameTime > 0.25 )
-            frameTime = 0.25;
-        currentTime = newTime;
-
-        accumulator += frameTime;
-
-        while ( accumulator >= dt )
-        {
-            previousState = currentState;
-            integrate( currentState, t, dt );
-            t += dt;
-            accumulator -= dt;
-        }
-
-        const double alpha = accumulator / dt;
-
-        State state = currentState * alpha +
-            previousState * ( 1.0 - alpha );
-
-        render( state );
-    }
-    */
 }
 
 pub fn create_components(ctx: &Context) -> bevy_ecs::world::World {
